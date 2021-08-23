d3b5b1d93b7fe35d1a335e946e46bcce450a27bb
Leonard <88027443+LeonardFritz@users.noreply.github.com>
2021-08-23 17:09:42 +0200

    Add files via upload

--- Landwirtschaftssimulator/src/aplication.java
+++ Landwirtschaftssimulator/src/aplication.java
@@ -4,26 +4,25 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
 import javax.imageio.ImageIO;
 
 public class aplication extends Application {
-	
-	static Movement movement = Movement.left;
-	
-	public enum Movement{
-		left,right, up, down
-	}
-
 
 	public static void main(String[] args) {
 		launch(args);
@@ -32,24 +31,13 @@ public class aplication extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-
-		Scene scene = new Scene(generateGamefield());  // Spielfeld erstellen
+		Player player = new Player(10, 10);								// Spieler erstellen
+		GridPane gridPane = generateGamefield();						// Spielfeld erstellen	
+		final Group group = new Group(gridPane, player);
+		Scene scene = new Scene(group);
 		
-		scene.addEventFilter(KeyEvent.KEY_PRESSED, key ->{
-			if(key.getCode() == KeyCode.W) {
-				movement = Movement.up;
-			}
-			if(key.getCode() == KeyCode.A) {
-				movement = Movement.left;
-			}
-			if(key.getCode() == KeyCode.S) {
-				movement = Movement.down;
-			}
-			if(key.getCode() == KeyCode.D) {
-				movement = Movement.right;
-			}
-		
-		});
+		movePlayerOnKeyPress(scene, player);
+		movePlayerOnMousePress(scene, player, createTransition(player));
 		
 		stage.setScene(scene);
 		stage.show();
@@ -57,32 +45,10 @@ public class aplication extends Application {
 	}
 	
 	public GridPane generateGamefield() {
-		
-		Player player = new Player(10, 8);
-		
 		GridPane grid = new GridPane();
-		
-		grid.addEventFilter(KeyEvent.KEY_PRESSED, key ->{
-			if(key.getCode() == KeyCode.W) {
-				movement = Movement.up;
-			}
-			if(key.getCode() == KeyCode.A) {
-				movement = Movement.left;
-			}
-			if(key.getCode() == KeyCode.S) {
-				movement = Movement.down;
-			}
-			if(key.getCode() == KeyCode.D) {
-				movement = Movement.right;
-			}
-		
-		});
-		
-		Image background = new Image("File:./Images/TestImage.png");	// Festes Spielfeld
-		ImageView backg = new ImageView(background);
-		
+
 		for (int i = 0; i < 30; i++) {
-            ColumnConstraints column = new ColumnConstraints(50);		// Spielfeldgr��e
+            ColumnConstraints column = new ColumnConstraints(50);		// Spielfeldgrösse
             grid.getColumnConstraints().add(column);
 		}
 		for (int i = 0; i < 21; i++) {
@@ -90,67 +56,75 @@ public class aplication extends Application {
             grid.getRowConstraints().add(row);
 		}
 		
+		Image background = new Image("File:./Images/TestImage.png");	// Festes Spielfeld
+		ImageView backg = new ImageView(background);
 		grid.add(backg, 0, 10);											// Landschaft
-		
-//		for(int j = 8; j <= 10; j++) {
-//		for (int i = 8; i <=23; i++) grid.add(new Field(), i, j);		// Feld1
-//		}
-//		for(int j = 12; j <= 17; j++) {
-//			for (int i = 8; i <=12; i++) grid.add(new Field(), i, j);	// Feld2
-//		}
-//		for(int j = 12; j <= 17; j++) {
-//			for (int i = 14; i <=23; i++) grid.add(new Field(), i, j);	// Feld 3
-//		}
-		
-//		Image bitmap = new Image("File:./Images/Bitmap.png");
-		File file = new File("Images/Bitmap.bmp");
+
+		File file = new File("Images/Bitmap.bmp");						// Weizenfelder
 		BufferedImage bitmap;
 		try {
 			bitmap = ImageIO.read(file);
 			for (int y = 0; y < bitmap.getHeight(); y++) {
 			    for (int x = 0; x < bitmap.getWidth(); x++) {
-			          int  clr   = bitmap.getRGB(x, y); 
-//			          System.out.println(clr);
-			          if(clr == -10728) {
-			        	  grid.add(new Field(), x, y);
-			          }
-//			          int  red   = (clr & 0x00ff0000) >> 16;
-//			          int  green = (clr & 0x0000ff00) >> 8;
-//			          int  blue  =  clr & 0x000000ff;
-//			          bitmap.setRGB(x, y, clr);
+//			          System.out.println(bitmap.getRGB(x, y));
+			          if(bitmap.getRGB(x, y) == -10728) { grid.add(new Field(), x, (y+1)); }
 			    }
 			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		
-		
-		
-		grid.add(player, player.getPosX(), player.getPosY());			// Spielfigur
-		
-		
-		switch(movement) {
-		case up:
-			player.relocate(player.getTranslateX(), player.getTranslateY()-1);
-			break;
-		case down:
-			player.relocate(player.getPosX(), player.getPosY()+1);
-
-			break;
-		case left:
-			player.relocate(player.getPosX()-1, player.getPosY());
-
-			break;
-		case right:
-			player.setLayoutX(player.getPosX()+1);
-
-			break;
-		}
-		
+		}	
+			
 		grid.setGridLinesVisible(true);
-		return grid;
-		
+		return grid;	
 	}
-
+	
+	private void movePlayerOnKeyPress(Scene scene, Player player) {		// TODO implement proper collision detection
+	    scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+	      @Override public void handle(KeyEvent event) {
+	        switch (event.getCode()) {
+	          case UP	, 	W	:  	player.setY(player.getY() - 50); break;
+	          case RIGHT,	D	: 	player.setX(player.getX() + 50); break;
+	          case DOWN	, 	S	: 	player.setY(player.getY() + 50); break;
+	          case LEFT	, 	A	: 	player.setX(player.getX() - 50); break;
+			default:
+				break;
+	        }
+	        if(player.getY() < 0) { player.setY(player.getY() + 50); 	}
+	        if(player.getX() < 0) { player.setX(player.getX() + 50); 	}
+	        if(player.getY() > 1050) { player.setY(player.getY() - 50); } // Window ist 1050px hoch
+	        if(player.getX() > 1500) { player.setX(player.getX() - 50); } // Window ist 1500px breit
+	      }
+	    });
+	  }
+	
+	private void movePlayerOnMousePress(Scene scene, final Player player, final TranslateTransition transition) {
+	    scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+	      @Override public void handle(MouseEvent event) {
+	    	  int newX = ((int) event.getSceneX() / 50) * 50 + 10; // Rundet Position der Maus ab und zentriert Spieler
+	    	  int newY = ((int) event.getSceneY() / 50) * 50 + 10; // 10 = (Feld.Breite - Spieler.Breite) / 2
+	        if (!event.isControlDown()) {
+	          player.setX(newX);
+	          player.setY(newY);
+	        } else {
+	          transition.setToX(newX - player.getX());
+	          transition.setToY(newY - player.getY());
+	          transition.playFromStart();
+	        }  
+	      }
+	    });
+	  }
+	
+	private TranslateTransition createTransition(final Player player) {
+	    final TranslateTransition transition = new TranslateTransition(Duration.seconds(0.25), player);
+	    transition.setOnFinished(new EventHandler<ActionEvent>() {
+	      @Override public void handle(ActionEvent event) {
+	        player.setX(player.getTranslateX() + player.getX());
+	        player.setY(player.getTranslateY() + player.getY());
+	        player.setTranslateX(0);
+	        player.setTranslateY(0);
+	      }
+	    });
+	    return transition;
+	  }
+	
 }
