a8a73a444627e75f93389a404d3301daa4f2d14f
Leonard <88027443+LeonardFritz@users.noreply.github.com>
2021-08-23 17:09:58 +0200

    Add files via upload

--- Landwirtschaftssimulator/src/resources/Player.java
+++ Landwirtschaftssimulator/src/resources/Player.java
@@ -2,37 +2,35 @@ package resources;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 
-public class Player extends ImageView {
-	
+public class Player extends ImageView {				// ImageView hat schon posX und posY + getter/setter
+													// also wieso diese überschreiben?
 	Image playerImage = new Image("File:./Images/PlayerTest.png");
 	
-	private int posX;
-	private int posY;
+//	private int posX;
+//	private int posY;
 	private boolean mounted = false;
 	
 	public Player(int posX, int posY){
-		this.posX = posX;
-		this.posY = posY;
+		this.setX(posX);
+		this.setY(posY);
 		this.setImage(playerImage);
 	}
-	public int getPosX() {
-		return posX;
-	}
+//	public int getPosX() {
+//		return posX;
+//	}
 
-	public void setPosX(int posX) {
-		this.posX = posX;
-	}
+//	public void setPosX(int posX) {
+//		this.posX = posX;
+//	}
 
-	public int getPosY() {
-		return posY;
-	}
-
-	public void setPosY(int posY) {
-		this.posY = posY;
-	}
+//	public int getPosY() {
+//		return posY;
+//	}
+//
+//	public void setPosY(int posY) {
+//		this.posY = posY;
+//	}
 	public boolean isMounted() {
 		return mounted;
 	}
