357d1770f13050b66df93d88c7e3a1b6bfb581b1
finitearth <t.zehle@gmail.com>
2021-08-27 13:29:00 +0200

    Update Tractor.java

--- Landwirtschaftssimulator/src/machinery/Tractor.java
+++ Landwirtschaftssimulator/src/machinery/Tractor.java
@@ -6,17 +6,38 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class Tractor extends Vehicle {
-	Image TractorViewL = new Image("File:./Images/TractorTest.png", 100, 100, false, false);
+	Image TractorViewA = new Image("File:./Images/TractorTestL.png", 100, 100, false, false);
+	Image TractorViewD = new Image("File:./Images/TractorTestR.png", 100, 100, false, false);
+	Image TractorViewW = new Image("File:./Images/TractorTestW.png", 100, 100, false, false);
+	Image TractorViewS = new Image("File:./Images/TractorTestS.png", 100, 100, false, false);
 	//Equiptment trailer;
 
 	public Tractor(int x, int y, int maxfuel) {
 		super(x, y, maxfuel);
 		this.setX(x);
 		this.setY(y);
-		this.setImage(TractorViewL);
+		this.setImage(TractorViewA);
+		
 		//trailer = null;
 
 	}
+
+		@Override
+		public void setImageW() {
+			this.setImage(TractorViewW);
+		}
+		@Override
+		public void setImageA() {
+			this.setImage(TractorViewA);
+		}
+		@Override
+		public void setImageS() {
+			this.setImage(TractorViewS);
+		}
+		@Override
+		public void setImageD() {
+			this.setImage(TractorViewD);
+		}
 
 	//public void equip(Equipment equipment) {
 		//if (trailer == null) {
