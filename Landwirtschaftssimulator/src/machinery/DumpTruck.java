df3dc7e87f6837fa7a4de8860e143aa2047d40a0
Lukas-B <bumueller.lukas@gmail.com>
2021-08-27 16:58:59 +0200

    Dump Truck

--- Landwirtschaftssimulator/src/machinery/DumpTruck.java
+++ Landwirtschaftssimulator/src/machinery/DumpTruck.java
@@ -8,15 +8,28 @@ public class DumpTruck extends ImageView{
 	private int load;
 	private int maxload;
 	
-	Image dumpTruckW = new Image("File:./Images/PlayerTest.png"); // TODO Bilder ersetzen
-	Image dumpTruckA = new Image("File:./Images/PlayerTest.png");
-	Image dumpTruckS = new Image("File:./Images/PlayerTest.png");
-	Image dumpTruckD = new Image("File:./Images/PlayerTest.png");
+	Image dumpTruckW = new Image("File:./Images/DumpTruckW.png"); // TODO Bilder ersetzen
+	Image dumpTruckA = new Image("File:./Images/DumpTruckA.png");
+	Image dumpTruckS = new Image("File:./Images/DumpTruckS.png");
+	Image dumpTruckD = new Image("File:./Images/DumpTruckD.png");
 	
 	public DumpTruck(int posX, int posY){
 		this.setX(posX);
 		this.setY(posY);
 		this.setImage(dumpTruckA);
+	}
+	
+	public void setImageW() {
+		this.setImage(dumpTruckW);
+	}
+	public void setImageA() {
+		this.setImage(dumpTruckA);
+	}
+	public void setImageS() {
+		this.setImage(dumpTruckS);
+	}
+	public void setImageD() {
+		this.setImage(dumpTruckD);
 	}
 
 	public int getLoad() {
