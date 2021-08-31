3f3d2eee5baae276d83d3776e17ab09c44e22df3
finitearth <t.zehle@gmail.com>
2021-08-31 10:47:32 +0200

    Update CollisionChecker.java
    Instead of taking player/vehicle as an argument, taking x/y positin as arguments. Turned off the bouncing against boundaries.

--- Landwirtschaftssimulator/src/Utils/CollisionChecker.java
+++ Landwirtschaftssimulator/src/Utils/CollisionChecker.java
@@ -24,9 +24,9 @@ public class CollisionChecker {
 		}
 		return d_x;
 }
-	public int collisioncheckX(Vehicle vehicle, int d_x) { //check for collision: if it collides, bounce back
-		int x = (int) vehicle.getX()+d_x;
-		int y = (int) vehicle.getY();
+	
+	public double collisioncheckX(double x, double y, double d_x) { //check for collision: if it collides, bounce back
+		x = x + d_x;
 		
 		for (ArrayList<Integer> boundarie : boundaries) {
 			int x1 = boundarie.get(0);
@@ -36,15 +36,14 @@ public class CollisionChecker {
 			
 			if ((x1<=x)&&(x2>=x)&&(y1<=y)&&(y2>=y)) {
 //				vehicle.setImageCollided();
-				return -1*d_x;
+				return 0;//-1*d_x;
 			}
 		}
 		return d_x;
 }
 	
-	public int collisioncheckY(Player player, int d_y) {
-		int x = (int) player.getX();
-		int y = (int) player.getY()+d_y;
+	public double collisioncheckY(double x, double y, double d_y) {
+		y = y + d_y;
 		
 		for (ArrayList<Integer> boundarie : boundaries) {
 			int x1 = boundarie.get(0);
@@ -53,30 +52,13 @@ public class CollisionChecker {
 			int y2 = boundarie.get(3);
 			
 			if ((x1<=x)&&(x2>=x)&&(y1<=y)&&(y2>=y)) {
-				player.setImageCollided();
-				return -1*d_y;
+				/* player.setImageCollided(); */
+				return 0;//-1*d_y;
 			}
 		}
 		return d_y;
 }
-	
-	public int collisioncheckY(Vehicle vehicle, int d_y) {
-		int x = (int) vehicle.getX();
-		int y = (int) vehicle.getY()+d_y;
-		
-		for (ArrayList<Integer> boundarie : boundaries) {
-			int x1 = boundarie.get(0);
-			int y1 = boundarie.get(1);
-			int x2 = boundarie.get(2);
-			int y2 = boundarie.get(3);
-			
-			if ((x1<=x)&&(x2>=x)&&(y1<=y)&&(y2>=y)) {
-//				vehicle.setImageCollided();
-				return -1*d_y;
-			}
-		}
-		return d_y;
-}
+
 	
 	
 	public void addboundary(int x1, int y1, int x2, int y2) {
