7081deb10db4bdf3175802f9c2f4662cc5db7e4f
finitearth <t.zehle@gmail.com>
2021-08-31 16:06:06 +0200

    Update DumpTruck.java

--- Landwirtschaftssimulator/src/machinery/DumpTruck.java
+++ Landwirtschaftssimulator/src/machinery/DumpTruck.java
@@ -1,33 +1,35 @@
 package machinery;
 
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
-public class DumpTruck extends ImageView{
-	
+public class DumpTruck extends Equipment {
+
 	private int load;
 	private int maxload;
-	
-	Image dumpTruckW = new Image("File:./Images/DumpTruckW.png"); 
+
+	Image dumpTruckW = new Image("File:./Images/DumpTruckW.png");
 	Image dumpTruckA = new Image("File:./Images/DumpTruckA.png");
 	Image dumpTruckS = new Image("File:./Images/DumpTruckS.png");
 	Image dumpTruckD = new Image("File:./Images/DumpTruckD.png");
-	
-	public DumpTruck(int posX, int posY){
+
+	public DumpTruck(int posX, int posY) {
 		this.setX(posX);
 		this.setY(posY);
 		this.setImage(dumpTruckA);
 	}
-	
+
 	public void setImageW() {
 		this.setImage(dumpTruckW);
 	}
+
 	public void setImageA() {
 		this.setImage(dumpTruckA);
 	}
+
 	public void setImageS() {
 		this.setImage(dumpTruckS);
 	}
+
 	public void setImageD() {
 		this.setImage(dumpTruckD);
 	}
@@ -35,12 +37,15 @@ public class DumpTruck extends ImageView{
 	public int getLoad() {
 		return load;
 	}
+
 	public void setLoad(int load) {
 		this.load = load;
 	}
+
 	public int getMaxload() {
 		return maxload;
 	}
+
 	public void setMaxload(int maxload) {
 		this.maxload = maxload;
 	}
