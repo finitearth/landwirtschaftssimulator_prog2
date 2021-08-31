d5efc33ca668683e79a0aee3b291263376d75673
juli220901 <88080591+juli220901@users.noreply.github.com>
2021-08-31 17:34:07 +0200

    Add files via upload
    Adding images to the buildings

--- Landwirtschaftssimulator/src/buildings/GasStation.java
+++ Landwirtschaftssimulator/src/buildings/GasStation.java
@@ -1,9 +1,20 @@
 package buildings;
 
 import Fields.Field;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
 
 public class GasStation extends Field{
 	int costPerLiter;
+	int cash;
+	Image GasStation  = new Image("File:./Images/Tanke.png", 50, 50, false, false);
+	
+	public GasStation(int posX, int posY) {
+		this.setX(posX);
+		this.setY(posY);
+		this.setImage(GasStation);
+	}
+
 	public int fuel(int amountOfFuel) {
 		if(cash >= (costPerLiter * amountOfFuel)) {
 			cash = cash - (costPerLiter * amountOfFuel);
--- Landwirtschaftssimulator/src/buildings/Landtrade.java
+++ Landwirtschaftssimulator/src/buildings/Landtrade.java
@@ -1,9 +1,19 @@
 package buildings;
 
 import Fields.Field;
+import javafx.scene.image.Image;
 
 public class Landtrade  extends Field{
 	int pricePerUnit;
+	int loading; //provisorisch
+	Image Landtrade  = new Image("File:./Images/Silo.png", 50, 50, false, false);
+	
+	public Landtrade(int posX, int posY) {
+		this.setX(posX);
+		this.setY(posY);
+		this.setImage(Landtrade);
+	}
+	
 	public int selling(int amountOfUnitsForSale) {
 		if(loading >= amountOfUnitsForSale) {    //loading muss noch definiert/angepasst werden
 			int payment = amountOfUnitsForSale * pricePerUnit;
