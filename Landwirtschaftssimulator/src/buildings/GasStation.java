975ece64f6fa6bc46b081a232b2f7525eee718c5
juli220901 <88080591+juli220901@users.noreply.github.com>
2021-09-28 15:18:15 +0200

    Update GasStation.java

--- Landwirtschaftssimulator/src/buildings/GasStation.java
+++ Landwirtschaftssimulator/src/buildings/GasStation.java
@@ -5,36 +5,66 @@ import machinery.Harvester;
 import machinery.Tractor;
 import settings.GameState;
 
+/**
+ * This class models the Gasstation. Particularly it refuels the Harvester and Tractor an checks if player has enough cash.
+ * 
+ * @author Julius Großmann
+ * @version 1.0
+ * 
+ */
 public class GasStation extends building {
-	int costPerLiter = 1;
 	Image GasStation = new Image("File:./Images/Tanke.png", 50, 50, false, false);
 	Harvester harvester;
 	Tractor tractor;
 	GameState save;
-
+	
+	/*
+	 * @param costPerLiter the Costs for one Liter fuel.
+	 */	
+	int costPerLiter = 1;
+	
+	/*
+	 * The constructor for the GasStation class. Calls the super constructor of
+	 * building. Sets the image.
+	 * 
+	 * @param int x the x coordinate of the tractor.
+	 * 
+	 * @param int y the y coordinate of the tractor.
+	 * 
+	 * @param GameState save the instance of the game state.
+	 */
 	public GasStation(int x, int y, GameState save) {
 		super(x, y);
 		this.setImage(GasStation);
 		this.save = save;
 	}
 
+	/*
+	* Refuels the tractor.
+	*
+	* @param GasStation gasStation the instance of the Gasstation
+	*
+	* @param Tractor tractor the instance of the Tractor
+	*
+	* @param int newFuel the amount of fuel that fits in the tractor
+	*/
 	public void refuelTractor(GasStation gasStation, Tractor tractor_) {
 		tractor = tractor_;
 		if (gasStation != null) {
-			int newFuel = tractor.maxfuel - tractor.fuel;
-			if (save.getCash() >= newFuel * costPerLiter) {
+			int newFuel = tractor.maxfuel - tractor.fuel;	//calculate the fuel for refulling
+			if (save.getCash() >= newFuel * costPerLiter) {		//is Cash enough to buy the fuel?
 				System.out.println(tractor.fuel);
-				save.setCash(save.getCash() - (newFuel * costPerLiter));
+				save.setCash(save.getCash() - (newFuel * costPerLiter));	//setting Cash
 				System.out.println("You have " + newFuel + " Liter refueled");
-				tractor.setFuel(10000);
+				tractor.setFuel(10000);		//setting fuel
 				System.out.print(save.getCash());
-			} else if (save.getCash() == 0) {
+			} else if (save.getCash() == 0) {	//is cash zero?
 				System.out.println("You have no money");
-			} else {
-				newFuel = save.getCash() * costPerLiter;
-				save.setCash(0);
+			} else {	//is cash not enough to buy the whole fuel needed and not zero?
+				newFuel = save.getCash() * costPerLiter;	//calculate the fuel for refulling
+				save.setCash(0);	//setting cash to zero
 				System.out.println("You have " + newFuel + " Liter refueled");
-				tractor.setFuel(tractor.getFuel() + newFuel);
+				tractor.setFuel(tractor.getFuel() + newFuel);	//setting fuel
 				System.out.print(save.getCash());
 			}
 		} else {
@@ -42,7 +72,7 @@ public class GasStation extends building {
 		}
 	}
 
-	public void refuelHarvester(GasStation gasStation, Harvester harvester_) {
+	public void refuelHarvester(GasStation gasStation, Harvester harvester_) { //same as refuelTractor just for the harvester
 		harvester = harvester_;
 		if (gasStation != null) {
 			int newFuel = harvester.maxfuel - harvester.fuel;
