6f968da239fd816bab8ebae59fd13ef2eea837dd
juli220901 <88080591+juli220901@users.noreply.github.com>
2021-09-28 15:02:35 +0200

    Update building.java

--- Landwirtschaftssimulator/src/buildings/building.java
+++ Landwirtschaftssimulator/src/buildings/building.java
@@ -2,11 +2,30 @@ package buildings;
 
 import javafx.scene.image.ImageView;
 
+/**
+ ** This class models a building. It acts as a parent class for the buildings
+ * Farmyard, Gasstation and Landtrade.
+ * 
+ * @author Julius Großmann
+ * @version 1.0
+ *
+ *
+ *
+ * 
+ */
 public class building extends ImageView {
 
 	public int x;
 	public int y;
 
+	/*
+	 * Constructor of the building class. Sets the x and y
+	 * coordinates.
+	 * 
+	 * @param x The x-Coordinate of the vehicle.
+	 * 
+	 * @param y The y-Coordinate of the vehicle.
+	 */
 	public building(int x_, int y_) {
 		setX(x_);
 		setY(y_);
